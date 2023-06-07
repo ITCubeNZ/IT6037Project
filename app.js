209a6f94fe56c0ba82f7a59cc600c05da70a73cf
Ben Olds <98551200+BenjaminCOlds@users.noreply.github.com>
2023-06-07 18:51:37 +1200

    File Setup
    Setup Files + added git ignore

---
+++ app.js
@@ -0,0 +1,41 @@
+// Default Configuration for Express & other Frameworks
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+
+// VIEWS
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/search', (req, res) => {
+    res.render('search')
+})
+
+app.get('/delete', (req, res) => {
+    res.render('delete')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.get('/modify', (req, res) => {
+    res.render('modify')
+})
+
+app.get('/add', (req, res) => {
+    res.render('add')
+})
+
+// RUN SERVER
+app.listen(port, () => {
+    console.log(`Great news! The application has started successfully on port ${port}`)
+})
