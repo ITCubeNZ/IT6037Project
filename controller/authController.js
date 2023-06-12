ff1f07e51ed26a58a6b3380decfe1d125e0a3560
Ben Olds <98551200+BenjaminCOlds@users.noreply.github.com>
2023-06-12 13:45:24 +1200

    Model Adjustments

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,6 @@
+const User = require("../models/User");
+
+
 module.exports.register_get = (req, res) => {
     res.render("register");
 }
@@ -10,8 +13,18 @@ module.exports.index_get = (req, res) => {
     res.render("index");
 }
 
-module.exports.register_post = (req, res) => {
-    res.send("User Registered");
+module.exports.register_post = async (req, res) => {
+    const { fullName, email, password, accountGroup } = req.body
+    
+    try {
+        const user = User.create({
+            fullName, email, password, accountGroup
+        })
+        res.status(201).json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send('error, user not created');
+    }
 }
 
 module.exports.login_post = (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,11 +1,27 @@
-const { Schema } = require("mongoose");
+const mongoose = require('mongoose');
 
-const userSchema = new Schema({
-    fullName: String, 
-    email: String, 
-    userGroup: String,
-    password: String
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true, 
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    }, 
+    userGroup: {
+        type: String
+    }
 })
 
-const User = model('User', userSchema);
-export default User;
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
