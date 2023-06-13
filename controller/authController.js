6246b815b171c2c9b8a0612cabddb9d035533ea8
Ben Olds <98551200+BenjaminCOlds@users.noreply.github.com>
2023-06-13 20:30:46 +1200

    Error handling and validation

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,26 @@
 const User = require("../models/User");
 
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { email: '', password: ''};
+    
+    // Check if email duplicate
+    if (err.code === 11000) {
+        errors.email = "that email is already registered";
+
+        return errors
+    }
+
+    // validation errors
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message
+        })
+    };
+
+    return errors;
+}
+
 
 module.exports.register_get = (req, res) => {
     res.render("register");
@@ -22,11 +43,13 @@ module.exports.register_post = async (req, res) => {
         })
         res.status(201).json(user);
     } catch (err) {
-        console.log(err);
-        res.status(400).send('error, user not created');
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
     }
 }
 
 module.exports.login_post = (req, res) => {
+    const { email, password } = req.body
+
     res.send("User has logged in");
 }
--- models/User.js
+++ models/User.js
@@ -1,21 +1,23 @@
 const mongoose = require('mongoose');
+const  { isEmail } = require('validator');
 
 
 const userSchema = new mongoose.Schema({
     fullName: {
         type: String,
-        required: true
+        required: [true, 'Please enter your full name.']
     },
     email: {
         type: String,
-        required: true, 
-        unique: true,
-        lowercase: true
+        required: [true, 'Please enter your email.'], 
+        unique: [true, 'Sorry, there is already a user registered with that email.'],
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email.']
     },
     password: {
         type: String,
-        required: true,
-        minLength: 6
+        required: [true, 'Please enter a password.'],
+        minlength: [6, "Sorry your password isn't long enough"]
     }, 
     userGroup: {
         type: String
