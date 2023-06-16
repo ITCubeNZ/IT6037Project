7932cdd42530970d0c471fe1427ea01536e77288
Ben Olds <98551200+BenjaminCOlds@users.noreply.github.com>
2023-06-16 22:00:18 +1200

    Authentication, added logout to navbar

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -20,25 +20,26 @@ const requireAuth = (req, res, next) => {
 }
 
 const checkUser = (req, res, next) => {
-    // Middleware used to check the current user.
     const token = req.cookies.jwt;
-
     if (token) {
         jwt.verify(token, secret, async (err, decodedToken) => {
-            if (err) {
-                next()
-            } else {
-                let user = await User.findById(decodedToken.id);
-                res.locals.current_user = user;
-                console.log(user)
-                next();
-            }
-        })
+        if (err) {
+            console.log(err);
+            res.locals.user = null;
+            next();
+        } else {
+            let user = await User.findById(decodedToken.id);
+            console.log(user)
+            res.locals.user = user;
+            next();
+        }
+    });
     } else {
+        console.log(1)
         res.locals.user = null;
         next();
     }
-}
+};
 
 const checkAuthenticated = (req, res, next) => {
     // Check if the user is authenticated, if the user is authenticated it will redirect to the search page, otherwise it will stay at index page. 
